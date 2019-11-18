d667dfe9afb3a20be73a63f7a15fbc5dcc5628ea
SashaPlavi <sasha.bingulac@gmail.com>
2019-11-18 11:26:29 +0100

    displying url

--- sever.js
+++ sever.js
@@ -2,9 +2,10 @@ const http = require("http");
 
 http
   .createServer((req, res) => {
-    res.writeHead(200);
+    res.writeHead(200, { "content-type": "text/html" });
 
     res.write("<h1>Hello Node!!!!</h1>\n");
+    res.write(`<h1>${req.url}</h1>`);
     res.end();
   })
   .listen(3000);
