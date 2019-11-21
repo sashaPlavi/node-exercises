abcd4170b282eef2aee7ccfe9ef2357e4f3eb0d4
SashaPlavi <sasha.bingulac@gmail.com>
2019-11-21 20:19:54 +0100

    individual members selected \

--- express.exercises/index.js
+++ express.exercises/index.js
@@ -1,9 +1,39 @@
 const express = require("express");
+const path = require("path");
+const members = require("./members");
+
+const loger = require("./midleware/loger");
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send("<h1>helooo</h1>");
+
+//init modleware
+app.use(loger);
+//geting all members
+app.get("/api/members", (req, res) => {
+  res.json(members);
 });
+// geting single member
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    //changing status code and eror mesage
+    res
+      .status(400)
+      .json({ msg: `no member with id ${req.params.id} was found` });
+  }
+});
+
+//sending file from a folder
+/*
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+*/
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 const port = process.env.port || 3000;
 
 app.listen(port, () => {
---
+++ express.exercises/members.js
@@ -0,0 +1,8 @@
+const members = [
+  { id: 1, name: "John", age: 30, status: "active" },
+  { id: 2, name: "John", age: 30, status: "active" },
+  { id: 3, name: "John", age: 30, status: "inactive" },
+  { id: 4, name: "John", age: 30, status: "active" }
+];
+
+module.exports = members;
