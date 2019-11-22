9d7c73ce34f1f88dd2ce1a26d3677a1657dfd501
SashaPlavi <sasha.bingulac@gmail.com>
2019-11-22 18:51:32 +0100

    making routs in separate file

---
+++ express.exercises/routes/api/members.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const members = require("../../members");
+//console.log(members);
+console.log(router);
+//console.log(express);
+
+//geting all members
+router.get("/", (req, res) => {
+  res.json(members);
+  console.log(req);
+
+  console.log("loging" + members);
+});
+// geting single member
+router.get("/:id", (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+  console.log(req);
+
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    console.log("bla");
+  } else {
+    //changing status code and eror mesage
+    res
+      .status(400)
+      .json({ msg: `no member with id ${req.params.id} was found` });
+    console.log("bla2");
+  }
+});
+
+module.exports = router;
