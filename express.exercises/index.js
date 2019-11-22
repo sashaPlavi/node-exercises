9536d56326e05703183aab3561ead8ee3ea92eac
SashaPlavi <sasha.bingulac@gmail.com>
2019-11-22 18:54:41 +0100

    making routs

--- express.exercises/index.js
+++ express.exercises/index.js
@@ -1,29 +1,12 @@
 const express = require("express");
 const path = require("path");
-const members = require("./members");
 
-const loger = require("./midleware/loger");
+//const loger = require("./midleware/loger");
 
 const app = express();
 
 //init modleware
-app.use(loger);
-//geting all members
-app.get("/api/members", (req, res) => {
-  res.json(members);
-});
-// geting single member
-app.get("/api/members/:id", (req, res) => {
-  const found = members.some(member => member.id === parseInt(req.params.id));
-  if (found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  } else {
-    //changing status code and eror mesage
-    res
-      .status(400)
-      .json({ msg: `no member with id ${req.params.id} was found` });
-  }
-});
+//app.use(loger);
 
 //sending file from a folder
 /*
@@ -32,7 +15,10 @@ app.get("/", (req, res) => {
 });
 */
 // set static folder
+
 app.use(express.static(path.join(__dirname, "public")));
+//routes
+app.use("/api/members", require("./routes/api/members"));
 
 const port = process.env.port || 3000;
 
