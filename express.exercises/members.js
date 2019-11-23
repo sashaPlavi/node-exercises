c93cf8af00505d0eec326515441d862240646236
SashaPlavi <sasha.bingulac@gmail.com>
2019-11-23 18:28:25 +0100

    change in memeber file

--- express.exercises/members.js
+++ express.exercises/members.js
@@ -1,8 +1,8 @@
 const members = [
-  { id: 1, name: "John", age: 30, status: "active" },
-  { id: 2, name: "John", age: 30, status: "active" },
-  { id: 3, name: "John", age: 30, status: "inactive" },
-  { id: 4, name: "John", age: 30, status: "active" }
+  { id: 1, name: "John", mail: "jo@gmail.com", status: "active" },
+  { id: 2, name: "mil", mail: "mi@gmail.com", status: "active" },
+  { id: 3, name: "fread", mail: "fre@gmail.com", status: "inactive" },
+  { id: 4, name: "mirko", mail: "rko@gmail.com", status: "active" }
 ];
 
 module.exports = members;
