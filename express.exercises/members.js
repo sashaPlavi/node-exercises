673f535b4a9be9f868f7b37b50400d306f05491c
SashaPlavi <sasha.bingulac@gmail.com>
2019-11-23 18:40:20 +0100

    deleting exercises

--- express.exercises/members.js
+++
@@ -1,8 +0,0 @@
-const members = [
-  { id: 1, name: "John", mail: "jo@gmail.com", status: "active" },
-  { id: 2, name: "mil", mail: "mi@gmail.com", status: "active" },
-  { id: 3, name: "fread", mail: "fre@gmail.com", status: "inactive" },
-  { id: 4, name: "mirko", mail: "rko@gmail.com", status: "active" }
-];
-
-module.exports = members;
